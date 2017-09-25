c46facdfdef3fddcf771751d6a540b465decb5a5
Ethan-Rivas <ethan-rivas@hotmail.com>
2017-09-25 15:49:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BotMsngr::Application.config.secret_key_base = '709e5bbb7ca9f519d017641a5b8a37623928be293587637f2ab3598e0132165ee2e1495de82e838791915835a17ace9a4b863a20c42c4bb2f19f221410165669'
