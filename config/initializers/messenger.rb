d43a989c84c8afe53f88e752473d173ee99c0ebf
Ethan-Rivas <ethan-rivas@hotmail.com>
2017-09-26 17:16:10 -0500

    config tokens

---
+++ config/initializers/messenger.rb
@@ -0,0 +1,4 @@
+Messenger.configure do |config|
+  config.verify_token      = 'tokenfortesting' # Cambiar por VERIFY_TOKEN de el webhook
+  config.page_access_token = 'tokenfortesting' # Cambiar por el ACCESS_TOKEN de la aplicacion
+end
