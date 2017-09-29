a0ad669cfa8cefebca268e279c52c8918db48685
Ethan-Rivas <ethan-rivas@hotmail.com>
2017-09-29 10:35:23 -0500

    changes for heroku deployment

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,4 +1,4 @@
 Messenger.configure do |config|
-  config.verify_token      = 'tokenfortesting' # Cambiar por VERIFY_TOKEN de el webhook
-  config.page_access_token = 'tokenfortesting' # Cambiar por el ACCESS_TOKEN de la aplicacion
+  config.verify_token      = 'randomtokenfortesting' # Cambiar por VERIFY_TOKEN de el webhook
+  config.page_access_token = 'EAAZAPcGZCoB0UBAPqxnAjfAIZAPwZACFCLOKiUkGOQcVZBxDV2df9HvAwsywAwrZCbYc5vC4osjfAEKloaZCBY7QzSXoZCdymXMbk786NDhVQoQCQHTIVfuJ4vi6xj9XWxrtuqhfObJjRdRsHjrF71YEyYIC7cZBGfqRGi7k2o8mo8wZDZD'
 end
