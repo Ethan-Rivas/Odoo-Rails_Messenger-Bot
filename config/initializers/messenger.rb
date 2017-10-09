d365b6df6fa37eeca555e6c04ec20c36410751c1
Ethan-Rivas <ethan-rivas@hotmail.com>
2017-10-09 11:44:23 -0500

    verify token por defecto

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,4 +1,3 @@
 Messenger.configure do |config|
-  config.verify_token      = 'randomtokenfortesting' # Cambiar por VERIFY_TOKEN de el webhook
-  config.page_access_token = 'EAAZAPcGZCoB0UBAPqxnAjfAIZAPwZACFCLOKiUkGOQcVZBxDV2df9HvAwsywAwrZCbYc5vC4osjfAEKloaZCBY7QzSXoZCdymXMbk786NDhVQoQCQHTIVfuJ4vi6xj9XWxrtuqhfObJjRdRsHjrF71YEyYIC7cZBGfqRGi7k2o8mo8wZDZD'
+  config.verify_token      = '' # Cambiar por VERIFY_TOKEN de el webhook
 end
